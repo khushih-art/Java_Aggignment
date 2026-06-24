b70d7fe1268d5c69ef036ad07a9e43a75d1a0677
khushih-art <khushihosamani367@gmail.com>
2026-06-24 10:12:02 +0530

    Add Printer class with method overloading

--- pro2.java
+++ pro2.java
@@ -1,1 +1,39 @@
+class Printer {
+
+    // Print integer
+    void print(int num) {
+        System.out.println("Integer: " + num);
+    }
+
+    // Print string
+    void print(String text) {
+        System.out.println("String: " + text);
+    }
+
+    // Print array of integers
+    void print(int arr[]) {
+        System.out.print("Array Elements: ");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+    }
+}
+
+public class program2 {
+    public static void main(String[] args) {
+
+        Printer p = new Printer();
+
+        // Calling overloaded methods
+        p.print(100);
+
+        p.print("Hello Java");
+
+        int numbers[] = {10, 20, 30, 40, 50};
+        p.print(numbers);
+    }
+}
 
