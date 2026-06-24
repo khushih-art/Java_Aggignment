3e77ba5d8c2d949f523850f4ba0d7a2dff15eb5e
khushih-art <khushihosamani367@gmail.com>
2026-06-24 10:13:59 +0530

    Add Display class with method overloading

--- pro6.java
+++ pro6.java
@@ -1,1 +1,23 @@
+class Display {
+    void show(int a) {
+        System.out.println("Integer: " + a);
+    }
 
+    void show(String s) {
+        System.out.println("String: " + s);
+    }
+
+    void show(double d) {
+        System.out.println("Double: " + d);
+    }
+}
+
+public class program6 {
+    public static void main(String[] args) {
+        Display obj = new Display();
+
+        obj.show(10);
+        obj.show("Java");
+        obj.show(25.5);
+    }
+}
