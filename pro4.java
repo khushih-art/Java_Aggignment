0e7a1dea922f27d752a78a49f413b092046373bd
khushih-art <khushihosamani367@gmail.com>
2026-06-24 10:13:06 +0530

    Add overloaded multiply methods for integers and doubles

--- pro4.java
+++ pro4.java
@@ -1,1 +1,30 @@
+public class program4 {
 
+    // Method to multiply two integers
+    int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Method to multiply three integers
+    int multiply(int a, int b, int c) {
+        return a * b * c;
+    }
+
+    // Method to multiply two floating-point numbers
+    double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        program4 obj = new program4();
+
+        System.out.println("Multiplication of two integers: " +
+                obj.multiply(5, 4));
+
+        System.out.println("Multiplication of three integers: " +
+                obj.multiply(2, 3, 4));
+
+        System.out.println("Multiplication of two floating-point numbers: " +
+                obj.multiply(2.5, 4.0));
+    }
+}
