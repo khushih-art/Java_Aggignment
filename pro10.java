b7d3bb82abc97bdce7e77817fc6138c307ff37c8
khushih-art <khushihosamani367@gmail.com>
2026-06-24 10:08:55 +0530

    Add Circle class implementing Printable and Shape

--- pro10.java
+++ pro10.java
@@ -1,1 +1,30 @@
+interface Printable {
+    void print();
+}
 
+abstract class Shape {
+    abstract void area();
+}
+
+class Circle extends Shape implements Printable {
+
+    double r = 5;
+
+    void area() {
+        System.out.println("Area = " + (3.14 * r * r));
+    }
+
+    public void print() {
+        System.out.println("Printing Circle Details");
+    }
+}
+
+public class program10 {
+    public static void main(String[] args) {
+
+        Circle c = new Circle();
+
+        c.area();
+        c.print();
+    }
+}
