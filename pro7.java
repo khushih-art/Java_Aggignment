e2e244a3492aa94cfb66dd1e3e698eebbf6302ca
khushih-art <khushihosamani367@gmail.com>
2026-06-24 10:14:25 +0530

    Implement class hierarchy with constructors and display method

--- pro7.java
+++ pro7.java
@@ -1,1 +1,30 @@
+class A {
+    A() {
+        System.out.println("Class A Constructor");
+    }
 
+    void display() {
+        System.out.println("Class A Method");
+    }
+}
+
+class B extends A {
+    B() {
+        super();
+        System.out.println("Class B Constructor");
+    }
+}
+
+class C extends B {
+    C() {
+        super();
+        System.out.println("Class C Constructor");
+    }
+}
+
+public class program7 {
+    public static void main(String[] args) {
+        C obj = new C();
+        obj.display();
+    }
+}
