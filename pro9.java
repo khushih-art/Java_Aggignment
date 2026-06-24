b9c6558ab4bb2f6ad2ed248b79f8672932e83d5f
khushih-art <khushihosamani367@gmail.com>
2026-06-24 10:15:12 +0530

    Add Employee classes with salary calculation

--- pro9.java
+++ pro9.java
@@ -1,1 +1,28 @@
+class Employee {
+    void calculateSalary() {
+        System.out.println("Employee Salary");
+    }
+}
 
+class FullTimeEmployee extends Employee {
+    void calculateSalary() {
+        System.out.println("Salary = Rs.50000");
+    }
+}
+
+class PartTimeEmployee extends Employee {
+    void calculateSalary() {
+        System.out.println("Salary = Rs.15000");
+    }
+}
+
+public class program9 {
+    public static void main(String[] args) {
+
+        FullTimeEmployee f = new FullTimeEmployee();
+        PartTimeEmployee p = new PartTimeEmployee();
+
+        f.calculateSalary();
+        p.calculateSalary();
+    }
+}
